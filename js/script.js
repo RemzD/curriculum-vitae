a772f557921c2fe6fe2e4fd1f7cbd3c84a861c3a
Remz_D <duez.remy@gmail.com>
2021-07-02 14:53:21 +0200

    adds avatar change when turning into dark mode

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const checkbox = document.getElementById("checkbox");
-let container = document.getElementsByClassName("container");
 
 checkbox.addEventListener("change", () => {
     // changes color theme
     document.querySelector(".container").classList.toggle("dark");
+    if (document.querySelector(".avatar").getAttribute("src") == "./images/avatar.jpg") {
+        document.querySelector(".avatar").setAttribute("src", "./images/avatar_glitch.gif");
+    } else {
+        document.querySelector(".avatar").setAttribute("src", "./images/avatar.jpg")
+    }
 })
